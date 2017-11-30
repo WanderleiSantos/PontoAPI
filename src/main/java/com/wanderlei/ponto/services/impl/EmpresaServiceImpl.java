5dde7a506dab7c8adfc2d8e8410588257f7a3f5b
Wanderlei Santos <wanderleisantosdias@gmail.com>
2017-11-30 21:00:22 -0200

    Services e Test dos Services

---
+++ src/main/java/com/wanderlei/ponto/services/impl/EmpresaServiceImpl.java
@@ -0,0 +1,34 @@
+package com.wanderlei.ponto.services.impl;
+
+import com.wanderlei.ponto.entities.Empresa;
+import com.wanderlei.ponto.repositories.EmpresaRepository;
+import com.wanderlei.ponto.services.EmpresaService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+
+@Service
+public class EmpresaServiceImpl implements EmpresaService {
+
+    private static final Logger log = LoggerFactory.getLogger(EmpresaServiceImpl.class);
+
+    @Autowired
+    private EmpresaRepository empresaRepository;
+
+    @Override
+    public Optional<Empresa> buscarPorCnpj(String cnpj) {
+        log.info("Buscando uma empresa para o CNPJ {}", cnpj);
+        return Optional.ofNullable(empresaRepository.findByCnpj(cnpj));
+    }
+
+    @Override
+    public Empresa persistir(Empresa empresa) {
+        log.info("Persistindo empresa: {}", empresa);
+        return this.empresaRepository.save(empresa);
+    }
+
+}
---
+++ src/test/java/com/wanderlei/ponto/utils/PasswordUtilsTest.java
@@ -0,0 +1,23 @@
+package com.wanderlei.ponto.utils;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordUtilsTest {
+    private static final String SENHA = "123456";
+    private final BCryptPasswordEncoder bCryptEncoder = new BCryptPasswordEncoder();
+
+    @Test
+    public void testSenhaNula() throws Exception {
+        assertNull(PasswordUtil.gerarBCrypt(null));
+    }
+
+    @Test
+    public void testGerarHashSenha() throws Exception {
+        String hash = PasswordUtil.gerarBCrypt(SENHA);
+
+        assertTrue(bCryptEncoder.matches(SENHA, hash));
+    }
+}
